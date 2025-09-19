767ef23715623df4a837fffef057e34b6e308a8c
perryofficial <naikprajwal20@gmail.com>
2025-09-19 14:50:51 +0530

    frontend + sql changes: change

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 // src/api.js
 import axios from 'axios';
 
-const API_BASE = process.env.REACT_APP_API_BASE || 'http://localhost:5000';
+const API_BASE = 'https://banking-system-5-varibles-changes.onrender.com';
 
 const api = axios.create({
   baseURL: API_BASE + '/api',
